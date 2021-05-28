85b272cae0d7b8d928ca91db5f85b0bf99632984
zaram <zaramenskiy@awada.ru>
2021-05-28 12:55:21 +0300

    new fili reader

--- FilmsCatalog/Controllers/FilmsController.cs
+++ FilmsCatalog/Controllers/FilmsController.cs
@@ -9,15 +9,92 @@ using FilmsCatalog.Data;
 using FilmsCatalog.Models;
 using Microsoft.AspNetCore.Http;
 
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+
 namespace TestAspFilm.Controllers
 {
-    public class FilmsController : Controller
+    public static class MultipartRequestHelper
     {
+        // Content-Type: multipart/form-data; boundary="----WebKitFormBoundarymx2fSWqWSd0OxQqq"
+        // The spec at https://tools.ietf.org/html/rfc2046#section-5.1 states that 70 characters is a reasonable limit.
+        public static string GetBoundary(MediaTypeHeaderValue contentType, int lengthLimit)
+        {
+            var boundary = HeaderUtilities.RemoveQuotes(contentType.Boundary).Value;
+
+            if (string.IsNullOrWhiteSpace(boundary))
+            {
+                throw new InvalidDataException("Missing content-type boundary.");
+            }
+
+            if (boundary.Length > lengthLimit)
+            {
+                throw new InvalidDataException(
+                    $"Multipart boundary length limit {lengthLimit} exceeded.");
+            }
+
+            return boundary;
+        }
+
+        public static bool IsMultipartContentType(string contentType)
+        {
+            return !string.IsNullOrEmpty(contentType)
+                   && contentType.IndexOf("multipart/", StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        public static bool HasFormDataContentDisposition(ContentDispositionHeaderValue contentDisposition)
+        {
+            // Content-Disposition: form-data; name="key";
+            return contentDisposition != null
+                && contentDisposition.DispositionType.Equals("form-data")
+                && string.IsNullOrEmpty(contentDisposition.FileName.Value)
+                && string.IsNullOrEmpty(contentDisposition.FileNameStar.Value);
+        }
+
+        public static bool HasFileContentDisposition(ContentDispositionHeaderValue contentDisposition)
+        {
+            // Content-Disposition: form-data; name="myfile1"; filename="Misc 002.jpg"
+            return contentDisposition != null
+                && contentDisposition.DispositionType.Equals("form-data")
+                && (!string.IsNullOrEmpty(contentDisposition.FileName.Value)
+                    || !string.IsNullOrEmpty(contentDisposition.FileNameStar.Value));
+        }
+    }
+
+   
+
+
+        public class FilmsController : Controller
+    {
+
         private readonly ApplicationDbContext _context;
 
         public FilmsController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        private static Encoding GetEncoding(MultipartSection section)
+        {
+            var hasMediaTypeHeader =
+                MediaTypeHeaderValue.TryParse(section.ContentType, out var mediaType);
+
+            // UTF-7 is insecure and shouldn't be honored. UTF-8 succeeds in 
+            // most cases.
+            if (!hasMediaTypeHeader || Encoding.UTF7.Equals(mediaType.Encoding))
+            {
+                return Encoding.UTF8;
+            }
+
+            return mediaType.Encoding;
         }
 
         // GET: Films
@@ -54,6 +131,7 @@ namespace TestAspFilm.Controllers
         }
 
         // GET: Films/CreateFilm
+        [HttpGet]
         public IActionResult CreateFilm()
         {
             ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
@@ -65,11 +143,83 @@ namespace TestAspFilm.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateFilm( IFormFile file)
+        public async Task<IActionResult> CreateFilm(byte[] p)
         {
-            Console.WriteLine("изображение получено");
+           
+
+            // Accumulate the form data key-value pairs in the request (formAccumulator).
+            var formAccumulator = new KeyValueAccumulator();
+            var trustedFileNameForDisplay = string.Empty;
+            var untrustedFileNameForStorage = string.Empty;
+            var streamedFileContent = Array.Empty<byte>();
+
+            var boundary = MultipartRequestHelper.GetBoundary(
+                MediaTypeHeaderValue.Parse(Request.ContentType),
+                1024);
+            var reader = new MultipartReader(boundary, HttpContext.Request.Body);
+
+            var section = await reader.ReadNextSectionAsync();
+
+            while (section != null)
+            {
+                var hasContentDispositionHeader =
+                    ContentDispositionHeaderValue.TryParse(
+                        section.ContentDisposition, out var contentDisposition);
+
+                if (hasContentDispositionHeader)
+                {
+                    if (MultipartRequestHelper
+                        .HasFileContentDisposition(contentDisposition))
+                    {
+                        untrustedFileNameForStorage = contentDisposition.FileName.Value;
+                        // Don't trust the file name sent by the client. To display
+                        // the file name, HTML-encode the value.
+                        trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                                contentDisposition.FileName.Value);
+                        var encoding = GetEncoding(section);
+
+                        using (var streamReader = new StreamReader(
+                            section.Body,
+                            encoding,
+                            detectEncodingFromByteOrderMarks: true,
+                            bufferSize: 1024,
+                            leaveOpen: true))
+                        {
+                            // The value length limit is enforced by 
+                            // MultipartBodyLengthLimit
+                            var value = await streamReader.ReadToEndAsync();
+
+                            if (string.Equals(value, "undefined",
+                                StringComparison.OrdinalIgnoreCase))
+                            {
+                                value = string.Empty;
+                            }
+
+                           
+
+                            if (formAccumulator.ValueCount >
+                                1024)
+                            {
+                                // Form key count limit of 
+                                // _defaultFormOptions.ValueCountLimit 
+                                // is exceeded.
+                                ModelState.AddModelError("File",
+                                    $"The request couldn't be processed (Error 3).");
+                                // Log error
+
+                                return BadRequest(ModelState);
+                            }
+                        }
+                    }
+                }
+
+                // Drain any remaining section body that hasn't been consumed and
+                // read the headers for the next section.
+                section = await reader.ReadNextSectionAsync();
+            }
 
             return NoContent();
+
         }
         // POST: Films/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
